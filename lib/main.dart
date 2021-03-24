d270e5bf9d8afd5719f4d58fce5ba53b4b4e757c
Chintan22 <Dankharachintan@gmail.com>
2021-03-24 19:17:34 -0400

    button_method_practice

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,32 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyBasicApp());
 
 class MyBasicApp extends StatelessWidget {
+  void buttonCheck() {
+    print('Option B selected with method pointer');
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Basic Quiz application'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Basic Quiz App'),
+        ),
+        body: Column(
+          children: [
+            Text('First sample question'),
+            RaisedButton(
+                child: Text('Select A'),
+                onPressed: () => print('Option A selected')),
+            RaisedButton(child: Text('Select B'), onPressed: buttonCheck),
+            RaisedButton(
+                child: Text('Select C'),
+                onPressed: () {
+                  print('Option C selected with anonymous function');
+                })
+          ],
+        ),
+      ),
     );
   }
 }
